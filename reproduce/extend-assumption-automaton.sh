e368da74de2b27f295bb608897a5f154a773a04a
Rodrigo Castaño <racicop@hotmail.com>
2016-02-23 16:49:49 -0300

    Changed to script printing traces outside the assumption automaton. Now accepting time limits.

--- reproduce/extend-assumption-automaton.sh
+++ reproduce/extend-assumption-automaton.sh
@@ -14,8 +14,10 @@ dirname_file=`dirname "$(readlink -f "$source_file")"`
 automaton_file_to_replace=../../output/AssumptionAutomaton.txt
 automaton_file_to_be_used=$dirname_file/AssumptionAutomaton.txt
 DIRNAME=$(output_dirname_extend $source_file)
+config=$CPACHECKER_PATH/test-configs/components/predicateAnalysis-generate-and-use-cmc-condition-print-calls.properties
 mkdir -p $DIRNAME
-sed -i 's@'$automaton_file_to_replace'@'$automaton_file_to_be_used'@' $CPACHECKER_PATH/test-configs/components/predicateAnalysis-generate-and-use-cmc-condition-print-calls.properties
-{ time ./scripts/cpa.sh -skipRecursion -outputpath $DIRNAME -logfile logfile.txt  -preprocess -config test-configs/components/predicateAnalysis-generate-and-use-cmc-condition-print-calls.properties $source_file; } > $DIRNAME/output.txt 2>&1
+sed -i 's@'$automaton_file_to_replace'@'$automaton_file_to_be_used'@' $config
+sed "s/.*limits.time.cpu.*/limits.time.cpu = $time_limit/" -i $config
+{ time ./scripts/cpa.sh -skipRecursion -outputpath $DIRNAME -logfile logfile.txt  -preprocess -config $config $source_file; } > $DIRNAME/output.txt 2>&1
 # Restore default assumption automaton filename
-sed -i 's@'$automaton_file_to_be_used'@'$automaton_file_to_replace'@' $CPACHECKER_PATH/test-configs/components/predicateAnalysis-generate-and-use-cmc-condition-print-calls.properties
+sed -i 's@'$automaton_file_to_be_used'@'$automaton_file_to_replace'@' $config
