aea5cd41f685f06556998655af02394a6abf3156
Rodrigo Castaño <racicop@hotmail.com>
2016-02-25 15:20:20 -0300

    Adding a script to swap true and false. Used to find traces that reach a safe (guaranteed not to produce assertion violations) state.

---
+++ reproduce/swap_false_and_true.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+file=$1
+# This is all a quick and dirty hack.
+# I believe there might be some optimizations to be made, by analyzing
+# graph reachability (that is, not actual reachability in the original
+# program) to the nodes false. The only tricky thing is to take special
+# care of conditions (syntax: "[condition]"), because in some cases
+# when there is branch branching and one of the branches has been
+# explored, the original condition might be removed and the "default"
+# GOTO __TRUE might be left.
+head -n 9 $file > true_false_defs.txt
+tail -n +10 $file > chopped_file.txt
+sed -i 's/__FALSE/__new_true/g' chopped_file.txt
+sed -i 's/__TRUE/__new_false/g' chopped_file.txt
+sed -i 's/__new_false/__FALSE/g' chopped_file.txt
+sed -i 's/__new_true/__TRUE/g' chopped_file.txt
+mv true_false_defs.txt $file
+cat chopped_file.txt >> $file
+rm chopped_file.txt
+
+
