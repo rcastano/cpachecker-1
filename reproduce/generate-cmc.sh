76b808bbc475808ded3fced533bf8e49f9168008
Rodrigo Castaño <racicop@hotmail.com>
2016-03-21 15:48:24 -0300

    Removed outdated CBMC time limit configuration.

--- reproduce/generate-cmc.sh
+++ reproduce/generate-cmc.sh
@@ -18,8 +18,6 @@ do
     mkdir -p $DIRNAME
     cp $file $DIRNAME
     sed "s/.*limits.time.cpu.*/limits.time.cpu = $time_limit/" -i $config
-    cbmc_time_limit=$[`echo $time_limit | sed 's/s//'`/10]s
-    sed "s/.*cbmc.timelimit.*/cbmc.timelimit = $cbmc_time_limit/" -i $config
     { time ./scripts/cpa.sh -skipRecursion -outputpath $DIRNAME -logfile logfile.txt  -preprocess -config $config $file; } > $DIRNAME/output.txt 2>&1
     rm $DIRNAME/*.dot
     rm $DIRNAME/*.json
