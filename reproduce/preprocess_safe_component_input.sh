23acd65a50adbb74bc25674e69e7c4655ba4e5bf
Rodrigo Castaño <racicop@hotmail.com>
2016-06-13 16:49:19 -0600

    Minor changes needed due to bugs when removing absolute paths.

--- reproduce/preprocess_safe_component_input.sh
+++ reproduce/preprocess_safe_component_input.sh
@@ -2,7 +2,7 @@
 
 # This script will create the .swapped version necessary to create the
 # safe component for all files in the directory containing our instances.
-for file in `find my-programs/fse12/ -type f -name \*assumption_automaton\*`; do
+for file in `find my-programs/fse12/ -type f -name \*assumption_automaton`; do
     ./reproduce/swap_false_and_true.sh $file; 
 done
 
