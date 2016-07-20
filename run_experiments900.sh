cdb46e224c65a93d3be44b1eb2bf29d66fb2d6bf
Rodrigo Castaño <racicop@hotmail.com>
2016-07-20 17:45:32 -0300

    Removing assumption automata files before running benchmark experiments.

--- run_experiments900.sh
+++ run_experiments900.sh
@@ -5,6 +5,14 @@ mkdir temp_benchexec_files
 # This should only run once
 mkdir results
 rm -rf temp_benchexec_files/*
+dir_backup=backup_$(date +%Y%m%d_%H%M%S)
+mkdir $dir_backup
+for file in $(find my-programs -type f -name \*assumption_automaton\*)
+do
+  dest_dir=$dir_backup/$(dirname $file)/
+  mkdir -p $dest_dir
+  mv $file $dest_dir
+done
 # Run initial verification phase (predicate analysis and
 # explicit value, separately)
 # For each instance.c file, this will generate the appropriate 
