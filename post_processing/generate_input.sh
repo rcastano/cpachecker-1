7a6799efd77b35dd9cb198b6b6bfe11e57b96aab
Rodrigo Castaño <racicop@hotmail.com>
2016-07-26 11:02:04 -0300

    Fixed minor bug

--- post_processing/generate_input.sh
+++ post_processing/generate_input.sh
@@ -2,7 +2,7 @@
 
 technique=$1
 instance=$2
-for witness_type in unexplored explored safe emptiness ;
+for witness_type in unexplored explored safe ;
 do
     ./post_processing/show_paths.sh `./post_processing/largest_lexicographic.sh $instance/produce-witnesses-$witness_type-$technique`/ > paths_$witness_type.txt
     ./post_processing/stack_depth.sh paths_$witness_type.txt > input_$witness_type.txt
--- run_experiments10800.sh
+++ run_experiments10800.sh
@@ -2,6 +2,7 @@
 if [ ! -z ${DO_NOT_ACTIVATE_BENCHEXEC+x} ]; then ./activate_benchexec.sh; fi
 # Create temp directory (clear contents in case it already existed).
 mkdir temp_benchexec_files
+# This should only run once
 mkdir results
 rm -rf temp_benchexec_files/*
 dir_backup=backup_$(date +%Y%m%d_%H%M%S)
@@ -21,10 +22,10 @@ cd cpachecker_files
 benchexec -o ../temp_benchexec_files --rundefinition generate-predicate --rundefinition generate-explicit --limitCores 1 ../experiments/experiment10800.xml
 results_file=`ls ../temp_benchexec_files/experiment10800*.txt`
 # Moving the files to the default results folder
-mv $results_file unified_results.txt
-# Leaving temp_benchexec_files/ empty.
-mv ../temp_benchexec_files/* ../results/
+cp $results_file ../unified_results.txt
 cd ../
+# Leaving temp_benchexec_files/ empty.
+mv ./temp_benchexec_files/* ./results/
 # Generating .swapped files, which have the TRUE and FALSE states
 # swapped. This step is necessary to generate "safe" components of
 # the execution reports.
@@ -32,7 +33,7 @@ cd ../
 # Generate all components
 cd cpachecker_files
 benchexec -o ../temp_benchexec_files \
---tasks original
+--tasks original \
 --rundefinition produce-witnesses-unexplored-predicate-from-predicate \
 --rundefinition produce-witnesses-unexplored-predicate-from-explicit \
 --rundefinition produce-witnesses-unexplored-explicit-from-predicate \
@@ -50,8 +51,9 @@ benchexec -o ../temp_benchexec_files \
 results_file=`ls ../temp_benchexec_files/experiment10800*.txt`
 # Appending results from the second phase to unified_results.txt
 cat $results_file >> ../unified_results.txt
+cd ../
 # Moving files to standard benchexec results folder.
-mv ../temp_benchexec_files/* ../results/
-cat ../unified_results.txt | python tables/completed_5percent.py 
-rm -rf ../temp_benchexec_files
+mv temp_benchexec_files/* results/
+cat unified_results.txt | python tables/completed_5percent.py 
+rm -rf temp_benchexec_files
 
