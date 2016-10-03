6bccb35dfaba3a19b260aa21bdfe4131f48156f6
Rodrigo Castaño <castano.rodrigo@gmail.com>
2016-10-03 07:41:40 -0300

    Minor change in benchexec setup script

--- setup_benchexec_systemd.sh
+++ setup_benchexec_systemd.sh
@@ -8,15 +8,12 @@ if [ $EUID -ne 0 ]; then
     echo "CAUTION! Run only at your own script after understanding the effects"
 fi
 
-ACTUAL_USER=$USER
-
 cp /etc/systemd/system.conf /etc/systemd/system.conf.backup.`date +"%Y-%m-%d_%H-%M-%S"`
 echo JoinControllers=cpuset,cpuacct,memory,freezer >> /etc/systemd/system.conf
 mv config_benchexec_systemd/benchexec-cgroup.service /etc/systemd/system/
 
 groupadd benchexec
-usermod -aG benchexec $ACTUAL_USER
+usermod -aG benchexec $USER
 
 update-initramfs -u
 echo "The changes require a system restart"
-exit
