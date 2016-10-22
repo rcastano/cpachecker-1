f8284b63993e08e7b19db24cd7b5797b1f2024ad
Rodrigo Castaño <racicop@hotmail.com>
2016-10-22 06:12:21 -0300

    Minor changes in Docker files

--- docker-img/download_repository.sh
+++ docker-img/download_repository.sh
@@ -2,5 +2,5 @@
 
 git clone https://github.com/rcastano/cpachecker-1.git /home/benchs
 cd /home/benchs
-git checkout 7eaf4ab3ce50827af9ae1a141852c97d75b360be
+git checkout ac49505ad832c3b96c9c96282d82c9f25ce3ad09
 
