52fd0efca763c3c861c2b6263223247260a4a672
Rodrigo Castaño <castano.rodrigo@gmail.com>
2016-11-07 16:13:42 -0300

    Minor fix

--- scripts/execution-reports/universal/all-cex-to-spec.sh
+++ scripts/execution-reports/universal/all-cex-to-spec.sh
@@ -9,7 +9,7 @@ folder=$1
 dest_folder=$2
 CEX=$(ls $folder/* | grep Counterexample.[^.]*.html)
 for cex in $CEX; do
-    number=$(echo $cex | sed 's/Counterexample.\([^.]*\).html/\1/')
+    number=$(echo $cex | sed 's/.*Counterexample.\([^.]*\).html/\1/')
     mkdir -p $dest_folder/cex$number/
     $SCRIPTPATH/get_json_from_html.sh $cex | \
         python $SCRIPTPATH/json_to_spec.py > \
