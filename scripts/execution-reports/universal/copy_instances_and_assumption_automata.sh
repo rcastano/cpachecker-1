e1e97e21ea63142efea476343528ddc0e5e42315
Rodrigo Castaño <castano.rodrigo@gmail.com>
2016-11-08 14:33:58 -0300

    Unified 2 scripts.

--- scripts/execution-reports/universal/copy_instances_and_assumption_automata.sh
+++ scripts/execution-reports/universal/copy_instances_and_assumption_automata.sh
@@ -18,6 +18,28 @@ then
     fi
 fi
 
+INSTANCES_DIR=$1
+UNIVERSAL_BENCH_DIR=$2
+
+instances=$(find $INSTANCES_DIR/ -type f -name \*.c)
+for name in $(ls $UNIVERSAL_BENCH_DIR/ | grep "\.c");
+do
+    instance=$(echo $instances | tr " " "\n" | grep $name)
+    cp $instance $UNIVERSAL_BENCH_DIR/$name/
+done
+
+for leaf in $(find $UNIVERSAL_BENCH_DIR/ -mindepth 2 -type d -name \*cex\*);
+do
+    pushd $leaf > /dev/null
+    instance=$(ls ../../*.c)
+    # pwd
+    cex_id=$(pwd | sed 's@.*cex\([^/]*\).*@\1@')
+    # echo $cex_id
+    # echo cex___$(echo $cex_id)___$(basename $instance)
+    ln -s $instance $technique___cex___$(echo $cex_id)___$(basename $instance)
+    popd > /dev/null
+done
+
 echo $technique >> $UNIVERSAL_BENCH_DIR/technique.txt
 
 instances=$(find $INSTANCES_DIR/ -type f -name \*.c)
@@ -28,3 +50,5 @@ do
         $instance.$technique.assumption_automaton   \
         $UNIVERSAL_BENCH_DIR/$name/assumption_automaton.txt
 done
+
+
