26056156df348a19117d894c3038238781581ec5
Rodrigo Castaño <racicop@hotmail.com>
2017-02-22 10:35:09 -0700

    Added missing functionality. Using os paths.

--- scripts/coverage/run_lower_bound_benchs.py
+++ scripts/coverage/run_lower_bound_benchs.py
@@ -23,25 +23,28 @@ def _script_path():
 class EmptyObject():
     pass
 
-def full_instance_pathname(instance):
-    raise Exception('not implemented yet')
+def full_instance_pathname(instances_root_dir, instance):
+    for root, dirs, files in os.walk(instances_root_dir):
+        if instance in files:
+            return os.path.abspath(os.path.join(root, instance)
+    raise Exception('Instance not found')
 
-def process_file(base, file, output_dir):
+def process_file(base, file, instances_root_dir, output_dir):
     try:
-        os.makedirs(output_dir + '/' + file)
+        os.makedirs(os.path.join(output_dir, file))
     except:
         raise Exception('Output directory already contains sub-directory for this filename: ' + file)
-    dir_to_process = base + '/' + file + '/'
+    dir_to_process = os.path.abspath(os.path.join(base, file))
     benchs_ran = os.listdir(dir_to_process)
     safe_benchs = [b for b in benchs_ran if 'safe' in b]
     safe_benchs.append(None)
     frontier_benchs = [b for b in benchs_ran if 'unexplored' in b]
     frontier_benchs.append(None)
     def find_single_subdir(base, bench):
-        sub_dirs = os.listdir(base + '/' + bench + '/')
+        sub_dirs = os.listdir(os.path.join(base, bench))
         # Make sure only one sub-directory exists
         assert len(sub_dirs) == 1
-        full_path = base + '/' + bench + '/' + sub_dirs[0] + '/'
+        full_path = os.path.join(base, bench, sub_dirs[0])
         return full_path
     
     def get_verification_technique(dir):
@@ -69,14 +72,14 @@ def process_file(base, file, output_dir):
             f = find_single_subdir(dir_to_process, f)
             args = EmptyObject()
             args.coverage_file = (s if s else f) + '/coverage.info'
-            args.instance_filename = full_instance_pathname(file)
+            args.instance_filename = full_instance_pathname(instances_root_dir, file)
             technique = verification_technique_s if s else verification_technique_f
             args.assumption_automaton_file = args.instance_filename + '.' + technique + '.assumption_automaton'
             args.safe_traces_dir = s
             args.frontier_traces_dir = f
             args.used_config_files = None
 
-            with open(output_dir + '/' + file + '/' + s_desc + '___' + f_desc + '.run', 'w') as f_out:
+            with open(os.path.join(output_dir, file, s_desc + '___' + f_desc + '.run'), 'w') as f_out:
                 start_time = time.time()
                 lower_bound_from_cex.main(args, f_out)
                 elapsed_time = time.time() - start_time
@@ -86,7 +89,7 @@ def process_file(base, file, output_dir):
 def main(args):
     all_files = os.listdir(args.benchexec_outputs)
     for file in all_files:
-        process_file(args.benchexec_outputs, file, args.output_dir)
+        process_file(args.benchexec_outputs, file, args.instances_root_dir, args.output_dir)
 
 
 
@@ -99,9 +102,12 @@ if __name__ == "__main__":
     parser.add_argument(
             "--output_dir",
             help="Root directory for output.")
+    parser.add_argument(
+            "--instances_root_dir",
+            help="Root directory for instances.")
 
     args = parser.parse_args()
-    if (not args.benchexec_outputs) or (not args.output_dir):
+    if not (args.benchexec_outputs and args.output_dir and args.instances_root_dir):
         parser.print_help()
     else:
         main(args)
