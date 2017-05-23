f45f28f9e0e6a7f532f630fb93fdc222ef98ca2d
Rodrigo Castaño <castano.rodrigo@gmail.com>
2017-05-23 16:53:54 -0300

    Added script used to produce latest bz2 files.

---
+++ run_heuristic.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+./scripts/benchmark.py \
+	-o temp_benchexec_files/ \
+	--limitCores 1 \
+	--tasks unfinished-predicate-900 \
+	--rundefinition coverage_explicit_coverage_heuristic_10_cex_900_secs \
+	experiments-xml/experiment900.xml
+./scripts/benchmark.py \
+	-o temp_benchexec_files/ \
+	--limitCores 1 \
+	--tasks unfinished-explicit-900 \
+	--rundefinition coverage_explicit_coverage_heuristic_10_cex_900_secs \
+	experiments-xml/experiment900.xml
+
+
