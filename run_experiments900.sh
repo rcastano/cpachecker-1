9e8ae6b861a7b9da344d0d7d297c822f158bc2ab
Rodrigo Castaño <racicop@hotmail.com>
2017-06-26 11:23:07 -0300

    Re using script for 2nd phase.

--- run_experiments900.sh
+++ run_experiments900.sh
@@ -45,41 +45,5 @@ cd ../
 # Leaving temp_benchexec_files/ empty.
 mv ./temp_benchexec_files/* ./results/
 
-# Generate all components
-cd cpachecker_files
-./scripts/benchmark.py -o ../temp_benchexec_files/ \
---tasks "from-predicate-900" \
---rundefinition produce-witnesses-unexplored-predicate-from-predicate \
---rundefinition produce-witnesses-unexplored-explicit-from-predicate \
---rundefinition produce-witnesses-explored-predicate-from-predicate \
---rundefinition produce-witnesses-explored-explicit-from-predicate \
---rundefinition produce-witnesses-safe-predicate-from-predicate \
---rundefinition produce-witnesses-safe-explicit-from-predicate \
---limitCores 1 ../experiments/experiment900.xml
-# Should be only one.
-results_file=`ls ../temp_benchexec_files/experiment900*.txt`
-# Appending results from the second phase to unified_results.txt
-cat $results_file >> ../unified_results.txt
-# Moving files to standard benchexec results folder.
-mv ../temp_benchexec_files/* ../results/
-
-./scripts/benchmark.py -o ../temp_benchexec_files/ \
---tasks "from-explicit-900" \
---rundefinition produce-witnesses-unexplored-predicate-from-explicit \
---rundefinition produce-witnesses-unexplored-explicit-from-explicit \
---rundefinition produce-witnesses-explored-predicate-from-explicit \
---rundefinition produce-witnesses-explored-explicit-from-explicit \
---rundefinition produce-witnesses-safe-predicate-from-explicit \
---rundefinition produce-witnesses-safe-explicit-from-explicit \
---limitCores 1 ../experiments/experiment900.xml
-# Should be only one.
-results_file=`ls ../temp_benchexec_files/experiment900*.txt`
-# Appending results from the second phase to unified_results.txt
-cat $results_file >> ../unified_results.txt
-# Moving files to standard benchexec results folder.
-mv ../temp_benchexec_files/* ../results/
-
-cd ../
-cat unified_results.txt | python tables/completed_5percent.py --time_limit 900 
-rm -rf temp_benchexec_files
-
+# Generate ER components (2nd phase)
+$SCRIPT_DIR/produce_er_experiments.sh
