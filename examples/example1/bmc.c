c0c629c9a0dea5dc141aa95b8288a9fdee03a5f1
Rodrigo Castaño <castano.rodrigo@gmail.com>
2017-06-26 16:15:18 -0300

    Minor changes to example, now it's the exact code on the paper.

--- examples/example1/bmc.c
+++ examples/example1/bmc.c
@@ -1,35 +1,32 @@
-int max(int a[], int n) {
-    int res = a[0];
-    for (int i=0;i<n;++i)
-      if (a[i] > res) res = a[i];
-    return res;
-}
-
+int __VERIFIER_nondet_int();
 int min(int a[], int n) {
     int res = a[0];
     for (int i=0;i<n;++i)
       if (a[i] < res) res = a[i];
     return res;
 }
-
+void assert(int cond);
+void init_vector(int a[], int n) {
+    int i = 0;
+    for (i = 0; i < n; ++i) {
+        a[i] = __VERIFIER_nondet_int();
+    }
+}
+// test_min
+void main(int large) {
+    int n;
+    if (large) {
+	n = 20;
+    } else {
+	n = 1;
+    }
+    int a[20];
+    init_vector(a, n);
+    int min_elem = min(a, n); 
+    assert(min_elem <= a[0]);
+}
 void assert(int cond) {
     if (!cond) {
 ERROR: goto ERROR;
     }
-}
-
-void init_vector(int a[], int n) {
-    int i = 0;
-    for (i = 0; i < n; ++i) {
-        a[i] = i;
-    }
-}
-
-void main() {
-    int a[4];
-    init_vector(a, 4);
-    int max_elem;
-    max_elem = max(a, 4);
-    int min_elem = min(a, 4); 
-    assert(min_elem <= max_elem);
 }
